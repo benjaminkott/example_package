5106159ad1b4f69f200f64051116fcaea3fe7afb
Benjamin Kott <benjamin.kott@wfp2.com>
2016-12-29 11:50:26 +0100

    [TASK] Use fluid styled content instead of css styled content

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.12-7.99.99',
-            'css_styled_content' => '6.2.0-7.99.99',
+            'typo3' => '7.6.0-8.99.99',
+            'fluid_styled_content' => '7.6.0-8.99.99',
         ),
         'conflicts' => array(
             'fluidpages' => '*',
