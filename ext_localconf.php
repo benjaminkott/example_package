f197d74c6bb0d76f92df5610be2b24efaf5c3e6b
Benjamin Kott <benjamin.kott@wfp2.com>
2017-01-05 23:53:21 +0100

    [TASK] Remove tsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,3 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'
-    . $_EXTKEY . '/Configuration/PageTS/TCEFORM.txt">'
-);
